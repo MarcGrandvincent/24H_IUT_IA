ecfe627659d721edd9ba76c6967a101470dbe681
Malo Mouorn <malo.mouron@gmail.com>
2024-05-04 14:35:01 +0200

    error de merde

--- 24H_IUT_IA/Models/Road.cs
+++ 24H_IUT_IA/Models/Road.cs
@@ -8,32 +8,32 @@ public class Road
     /// <summary>
     /// Gets or sets the level of the boat.
     /// </summary>
-    public int LevelBoat{ get; set; };
+    public int LevelBoat{ get; set; }
 
     /// <summary>
     /// Gets or sets the attack value of the merchant ship.
     /// </summary>
-    public int AttackValue{ get; set; };
+    public int AttackValue{ get; set; }
 
     /// <summary>
     /// Gets or sets the value of the first chest.
     /// </summary>
-    public int ChestValue1{ get; set; };
+    public int ChestValue1{ get; set; }
 
     /// <summary>
     /// Gets or sets the value of the second chest.
     /// </summary>
-    public int ChestValue2{ get; set; };
+    public int ChestValue2{ get; set; }
 
     /// <summary>
     /// Gets or sets the value of the third chest.
     /// </summary>
-    public int ChestValue3{ get; set; };
+    public int ChestValue3{ get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether a monster is present on the road.
     /// </summary>
-    public bool IsMonsterPresent { get; set; };
+    public bool IsMonsterPresent { get; set; }
     
     /// <summary>
     /// Initializes a new instance of the <see cref="Road"/> class.
