c5b30361ba5576f1f5a141869c2c3c6f6bc3f7bd
MarcGrandvincent <marc.grandvincent@iut-dijon.u-bourgogne.fr>
2024-05-04 18:43:38 +0200

    Méthode pour récupérer les coffres

--- 24H_IUT_IA/AI/Services/MemoryService.cs
+++ 24H_IUT_IA/AI/Services/MemoryService.cs
@@ -7,37 +7,39 @@ namespace _24H_IUT_IA.AI.Services;
 /// </summary>
 public class MemoryService
 {
+    public bool PlayersInitialized { get; set; }
     public string? TeamName { get; set; } = null;
-    
-    private int _teamNumber  = 0;
-    public List<Player> Players { get; set; } = new List<Player>();
+
+    private int _teamNumber = 0;
+    public List<Player> Players { get; set; } = [new(), new(), new(), new()];
     public List<Road> Roads { get; set; } = new List<Road>();
-    
+
     public int Turn { get; set; } = 0;
 
     public Player GetOurPlayerInfo()
     {
-        Console.WriteLine(Players.Count);
-        Console.WriteLine(_teamNumber);
         return Players[_teamNumber - 1];
     }
-    
+
     public void ParserPlayersInfo(string messageReceived)
     {
+        PlayersInitialized = true;
+        
         var players = messageReceived.Split("|");
 
-        foreach (var player in players)
+        for (var index = 0; index < players.Length; index++)
         {
-            Players.Add(new Player(player));
+            var player = players[index];
+            Players[index].MapPlayerData(player);
         }
     }
-    
+
     public void ParseRouteInfo(string messageReceived)
     {
         var roads = messageReceived.Split("|");
 
         Road? lastRoad = null;
-        
+
         foreach (var road in roads)
         {
             if (!(road is "True" or "False"))
@@ -45,11 +47,10 @@ public class MemoryService
                 lastRoad = new Road(road);
                 Roads.Add(lastRoad);
             }
-            else
-                if (lastRoad != null) lastRoad.IsMonsterPresent = road == "True";
+            else if (lastRoad != null) lastRoad.IsMonsterPresent = road == "True";
         }
     }
-    
+
     public void ParseTeamNumber(string messageReceived)
     {
         _teamNumber = int.Parse(messageReceived.Split("|")[1]);
