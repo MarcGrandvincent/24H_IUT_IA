327a06302e8aa8c5bd78e2e97296e3bdbdced671
mathis-de-sousa-pro <mathis.dsa39@gmail.com>
2024-05-04 21:12:38 +0200

    presque fin de FairAI.cs

--- 24H_IUT_IA/AI/Decisions/FairAI.cs
+++ 24H_IUT_IA/AI/Decisions/FairAI.cs
@@ -2,101 +2,148 @@
 
 namespace _24H_IUT_IA.AI.Decisions;
 
+/// <summary>
+///     This class represents a decision-making service for an AI.
+///     It contains methods for determining the best action to take based on the current state of the game.
+/// </summary>
 public class FairAI(AI ai) : DecisionMakingService(ai)
 {
-    // définition de 4 scores pour chaque action
-    public int RepareScore { get; set; }
-    public int RecellScore { get; set; }
-    public int BetrayScore { get; set; }
-    public int AttackScore { get; set; }
-
+    // Dictionary containing the score for each action and the key is name of the action
+    public Dictionary<string, int> ListScore = new Dictionary<string, int>
+    {
+        {"BETRAY", 0},
+        {"ATTACK", 0},
+        {"REPAIRE", 0},
+        {"RESELL", 0}
+    };
+    /// <summary>
+    ///     Determines the next action to take based on the last received message.
+    /// </summary>
+    /// <param name="lastReceivedMessage">The last message received from the game server.</param>
+    /// <returns>The action to take as a string, or null if no action should be taken.</returns>
     public override string? TakeNewAction(string lastReceivedMessage)
     {
-        //appel de la methode commune à tous les débuts de tour
+        // Call the common method for all turn starts
         if (StartTurn(lastReceivedMessage) is not null || !OurTurn)
             return StartTurn(lastReceivedMessage);
-        else
-        {
-            //debut de drunken ai 
-            return WorkForAction(lastReceivedMessage);
-        }
+        // Start of drunken ai 
+        return WorkForAction(lastReceivedMessage);
     }
 
     /// <summary>
-    /// logic de flair ai 
+    ///     Logic for the Fair AI.
     /// </summary>
-    /// <param name="lastReceivedMessage">dernier message reçu</param>
-    /// <returns></returns>
+    /// <param name="lastReceivedMessage">The last message received from the game server.</param>
+    /// <returns>The action to take as a string, or null if no action should be taken.</returns>
     private string? WorkForAction(string lastReceivedMessage)
     {
-        return lastReceivedMessage;
+        //remplis le dictionnaire avec les données des 4 tests
+        FillListScore();
+        //retourne l'action avec le score le plus élevé
+        return ListScore.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+        
+    }
+
+    private void FillListScore()
+    {
+        ListScore["REPAIRE"] = RepareGen();
+        ListScore["RESELL"] = ResellGen();
+        (ListScore["BETRAY"], _) = BetrayGen();
+        ListScore["ATTACK"] = TestRoute().Item2;
     }
 
     /// <summary>
-    /// réparer le bateau dans le cas général
+    ///     Determines whether to repair the boat in a general case.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>The score for the repair action.</returns>
     public int RepareGen()
     {
-        if (this.Ai.MemoryService.GetOurPlayerInfo().Life > 3 || this.Ai.MemoryService.Turn == 120)
+        if (Ai.MemoryService.GetOurPlayerInfo().Life > 3 || Ai.MemoryService.Turn == 120)
             return 0;
-        else
-            return 100;
-        //si on a plus de 3(ou 2(à tester)) pv ou si c'est le detrnier tour on renvoie 0 sinon on renvoie 100
-        return 0;
+        return 100;
     }
 
     /// <summary>
-    /// détermine si on doit revendre nos coffres dans un cas général
+    ///     Determines whether to sell our chests in a general case.
     /// </summary>
-    /// <returns></returns>
-    public int RecellGen()
+    /// <returns>The score for the sell action.</returns>
+    public int ResellGen()
     {
-        //si notre liste de coffres Chests est vide on renvoie 0
-        if (this.Ai.MemoryService.GetOurPlayerInfo().Chests.Count == 0 || this.Ai.MemoryService.Turn == 0 ||
-            this.Ai.MemoryService.Turn == 120)
+        if (Ai.MemoryService.GetOurPlayerInfo().Chests.Count == 0 || Ai.MemoryService.Turn == 0 ||
+            Ai.MemoryService.Turn == 120)
             return 0;
 
-        // sinon si Chests.Count() = 5 (si on a le max de coffres) ou si c'est le dernier tour on renvoie 100
-        if (this.Ai.MemoryService.GetOurPlayerInfo().Chests.Count == 5 || this.Ai.MemoryService.Turn == 100)
+        if (Ai.MemoryService.GetOurPlayerInfo().Chests.Count == 5 || Ai.MemoryService.Turn == 100)
             return 100;
 
-        // sinon => le butin général * (1/2 + l'ordre du joueur/8)
-        else
-            return (int) (this.Ai.MemoryService.GetOurPlayerInfo().LootValue *
-                          (0.5 + this.Ai.MemoryService.GetOurPlayerInfo().Order / 8));
-        return 0;
+        return (int)(Ai.MemoryService.GetOurPlayerInfo().LootValue *
+                     (0.5 + Ai.MemoryService.GetOurPlayerInfo().Order / 8));
     }
 
     /// <summary>
-    /// détermine si on doit trahir un joueur dans un cas général
+    ///     Determines whether to betray a player in a general case.
     /// </summary>
+    /// <returns>A tuple containing the score for the betray action and the player to betray.</returns>
     public (int, int) BetrayGen()
     {
-        (int, int) ret = (0, 0);
-        foreach (var player in this.Ai.MemoryService.Players)
-        {
-            if (player.Order != this.Ai.MemoryService.GetOurPlayerInfo().Order)
+        var ret = (0, 0);
+        foreach (var player in Ai.MemoryService.Players)
+            if (player.Order != Ai.MemoryService.GetOurPlayerInfo().Order)
             {
- 
             }
-        }
 
         return (0, 0);
     }
+    
 
     /// <summary>
-    /// détermine si on doit attaquer une route commerciale
+    ///     Tests a route for potential attack.
     /// </summary>
-    public int Attack()
+    /// <returns>A tuple containing the score for the attack action and the route to attack.</returns>
+    public (int, int) TestRoute()
     {
-        return 0;
+        var ret = (0, 0);
+        foreach (var route in Ai.MemoryService.Roads)
+            if (route.AttackValue <= Ai.MemoryService.GetOurPlayerInfo().AttackValue)
+                if (ret.Item2 < MaxChest(Ai.MemoryService.Roads.IndexOf(route) + 1))
+                {
+                    ret.Item2 = MaxChest(Ai.MemoryService.Roads.IndexOf(route) + 1);
+                    ret.Item1 = Ai.MemoryService.Roads.IndexOf(route) + 1;
+                }
+
+        return ret;
     }
-    
+
     /// <summary>
-    /// détermine si c'est notre tour
+    ///     Returns the number of the route based on the route parameter.
     /// </summary>
-    /// <returns>renvoie l'ordre de passage de notre joueur </returns>
-    private int DefOurTurn() => this.Ai.MemoryService.Turn % this.Ai.MemoryService.TeamNumber;
-    
+    /// <param name="nbRoad">The route to test.</param>
+    /// <returns>The maximum chest value for the route.</returns>
+    public int MaxChest(int nbRoad)
+    {
+        var c = 0;
+        var var = 0;
+        foreach (var joueur in Ai.MemoryService.Players)
+            if (joueur.Activity.ToString() == $"PILLER{nbRoad}")
+                c++;
+
+        if (c < 3)
+            switch (c + 1)
+            {
+                case 1:
+                    var = Ai.MemoryService.Roads[nbRoad].ChestValue1;
+                    break;
+                case 2:
+                    var = Ai.MemoryService.Roads[nbRoad].ChestValue2;
+                    break;
+                case 3:
+                    var = Ai.MemoryService.Roads[nbRoad].ChestValue3;
+                    break;
+            }
+
+        if (Ai.MemoryService.Roads[nbRoad].IsMonsterPresent)
+            var = (int)(var * 0.6);
+
+        return var;
+    }
 }
