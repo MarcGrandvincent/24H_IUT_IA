572d3cebed6b2ec4ef14acd16a8a3799fd294fe2
mathis-de-sousa-pro <mathis.dsa39@gmail.com>
2024-05-04 14:14:42 +0200

    rajout de tous les messages

--- 24H_IUT_IA/Models/Messages.cs
+++ 24H_IUT_IA/Models/Messages.cs
@@ -4,4 +4,18 @@ public static class Messages
 {
     public const string Start = "START";
     public const string End = "END";
+    public const string TeamName = "NOM_EQUIPE";
+    public const string Welcome = "Bonjour";
+    public const string StartTurn = "DEBUT_TOUR";
+    public const string EndGame = "FIN";
+    public const string Command = "NomCommande";
+    public const string Pillage = "PILLER";
+    public const string Repair = "REPARER";
+    public const string Fence = "RECELER";
+    public const string Recruit = "RECRUTER";
+    public const string Betray = "TRAHIR";
+    public const string PlayersInfo = "JOUEURS";
+    public const string RoutesInfo = "ROUTES";
+    public const string Ok = "OK";
+    public const string NotOk = "NOK";
 }
